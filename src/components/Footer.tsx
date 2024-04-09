1681a2db1d626308ba02a6bd861c89807b1de256
Sawan Sunar <Sawansunar24072002@gmail.com>
2024-04-09 16:03:30 +0530

    complete home page done

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,28 @@
 function Footer() {
+    const data = [
+        "Things that I love to see",
+        "More of the stuff in the world",
+        "Lots of things",
+        "Science is fun to listen too",
+        "Programming is hard for beginners",
+    ]
+
     return (
         <div class="footer-section">
-            hello there
+            <div class="scroller" data-animated="true">
+                <ul class="scroller-inner">
+                    {data.map((quote, _) => (
+                        <li>
+                            {quote}
+                        </li>
+                    ))}
+                    {data.map((quote, _) => (
+                        <li>
+                            {quote}
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
     );
 }
